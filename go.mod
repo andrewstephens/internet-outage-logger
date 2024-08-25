65ef2c7122bc8ad7a205e206a7734967a1399154
Andrew Stephens <digitalmk@gmail.com>
2024-08-25 12:59:50 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module InternetConnection
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	defaultCheckInterval = 10
+	defaultLogFile       = "connection_log.txt"
+	testURL              = "http://www.google.com"
+)
+
+func main() {
+	intervalPtr := flag.Int("interval", defaultCheckInterval, "Check interval in seconds")
+	flag.IntVar(intervalPtr, "i", defaultCheckInterval, "Check interval in seconds (shorthand)")
+
+	logFilePtr := flag.String("logfile", defaultLogFile, "Log file path")
+	flag.StringVar(logFilePtr, "l", defaultLogFile, "Log file path (shorthand)")
+
+	flag.Parse()
+
+	checkInterval := time.Duration(*intervalPtr) * time.Second
+
+	file, err := os.OpenFile(*logFilePtr, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	logger := log.New(file, "", log.LstdFlags)
+
+	fmt.Printf("Monitoring internet connection. Check interval: %v, Log file: %s\n", checkInterval, *logFilePtr)
+
+	var lastStatus bool
+	for {
+		connected := checkConnection()
+
+		if !connected && lastStatus {
+			logger.Println("Internet connection lost")
+			fmt.Println("Internet connection lost")
+		} else if connected && !lastStatus {
+			logger.Println("Internet connection restored")
+			fmt.Println("Internet connection restored")
+		}
+
+		lastStatus = connected
+		time.Sleep(checkInterval)
+	}
+}
+
+func checkConnection() bool {
+	_, err := http.Get(testURL)
+	return err == nil
+}
